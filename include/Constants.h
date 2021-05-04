bca415656f75e31f2224e3fd05124e94479b823c
kamelfakihh <kamelfakihh@gmail.com>
2021-05-05 01:52:00 +0300

    changed collision area size

--- include/Constants.h
+++ include/Constants.h
@@ -4,10 +4,10 @@
 #define SCRN_W 800          // screen width
 #define SCRN_H 600          // screen height
 
-#define R_WALL_POS 750      // right wall position
-#define L_WALL_POS 50       // left wall position
+#define R_WALL_POS 800      // right wall position
+#define L_WALL_POS 0       // left wall position
 
-#define U_WALL_POS 50       // up wall position
-#define D_WALL_POS 550      // down wall position
+#define U_WALL_POS 0       // up wall position
+#define D_WALL_POS 600      // down wall position
 
 #endif
